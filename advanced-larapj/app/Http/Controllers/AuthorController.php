750d3b323f8e00bc6e54ce9d5db50ecd5abc0c2c
takuma1156 <takumaujiie0906@yahoo.co.jp>
2022-12-28 13:20:41 +0900

    Eloquent

---
+++ advanced-larapj/app/Http/Controllers/AuthorController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Author;
+use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+    public function index()
+    {
+        $authors = Author::all();
+        return view('index', ['authors' => $authors]);
+    }
+
+    public function find()
+    {
+        return view('find', ['input' => '']);
+    }
+
+    public function search(Request $request)
+    {
+        $author = Author::find($request->input);
+        $param = [
+        'author' => $author,
+        'input' => $request->input
+        ];
+        return view('find', $param);
+    }
+}
--- advanced-larapj/routes/web.php
+++ advanced-larapj/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthorController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AuthorController::class, 'index']);
+Route::get('/find', [AuthorController::class, 'find']);
+Route::post('/find', [AuthorController::class, 'search']);
