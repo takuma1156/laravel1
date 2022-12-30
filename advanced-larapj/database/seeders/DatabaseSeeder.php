99cd3cddd6b1a5c982fef7483f701462d1436cce
takuma1156 <takumaujiie0906@yahoo.co.jp>
2022-12-30 11:39:02 +0900

    test

--- advanced-larapj/database/seeders/DatabaseSeeder.php
+++ advanced-larapj/database/seeders/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(BooksTableSeeder::class);
+        \App\Models\Author::factory(10)->create();
     }
 }
